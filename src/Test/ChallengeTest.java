046b6dd83b43b89e649c9639f7c63d78718a478b
Gustav Strömbeck <gustav.strombeck.8367@student.uu.se>
2025-09-15 14:00:18 +0200

    added tests for rooms, also added simulated tests for coming updates, they are commented away for now

---
+++ src/Test/ChallengeTest.java
@@ -0,0 +1,28 @@
+//package.Room;
+//package.ChallengeRoom;
+
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Simulated test class created to show how and what the test is supposed to test for
+ * in future implementation. // dessa simuleringar har kommenterats bort tillsvidare
+ */
+
+//class ChallengeRoomTest {
+
+  //  @Test
+    //void exitIsLockedUntilSolved() {
+       // ChallengeRoom room = new ChallengeRoom("riddle room", "piano");
+     //   Room treasure = new Room("treasure");
+       // room.setExit("east", treasure);
+
+       // assertNull(room.getExit("east"));
+       // assertFalse(room.attempt("keyboard"));
+       // assertTrue(room.attempt("piano"));
+       // assertEquals(treasure, room.getExit("east"));
+    //}
+//}
---
+++ src/Test/MonsterTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Simulated test class to show how and what the class is supposed to test upon
+ * implementation in future update of the game. // dessa simuleringar har kommenterats bort tillsvidare
+ */
+//class MonsterRoomTest {
+
+   // @Test
+   // void correctAnswerDamagesMonster() {
+   //     Player player = new Player(30);      // start-HP
+    //    Monster monster = new Monster(20);   // monster-HP
+      //  MonsterRoom room = new MonsterRoom(monster, player);
+
+      //  room.attack("2+3", "5");  // right answer
+
+      //  assertTrue(monster.getHp() < 20,
+                //"Monster takes damage with right answer");
+      //  assertEquals(30, player.getHp(),
+       //         "Player takes damage with wrong answer");
+    //}
+
+   // @Test
+   // void wrongAnswerDamagesPlayer() {
+    //    Player player = new Player(30);
+     //   Monster monster = new Monster(20);
+       // MonsterRoom room = new MonsterRoom(monster, player);
+
+       // room.attack("2+3", "7");  // wrong answer
+
+      //  assertTrue(player.getHp() < 30,
+             //   "Player takes damage with wrong answer");
+      //  assertEquals(20, monster.getHp(),
+             //   "Monster does not takes damage with wrong answer");
+   // }
+
+  //  @Test
+    //void monsterDefeatedMarksRoomCleared() {
+      //  Player player = new Player(30);
+        //Monster monster = new Monster(10);
+        //MonsterRoom room = new MonsterRoom(monster, player);
+
+        // Answer correctly until monster dies
+        //while (!monster.isDead()) {
+         //   room.attack("1+1", "2");
+       // }
+
+        //assertTrue(monster.isDead(),
+            //    "Monstret has to be out of health");
+      //  assertTrue(room.isCleared(),
+             //   "Room is marked as cleared when monster is dead");
+   // }
+
+  //  @Test
+    //void playerDiesIfTooManyWrongAnswers() {
+      //  Player player = new Player(10);
+        //Monster monster = new Monster(20);
+        //MonsterRoom room = new MonsterRoom(monster, player);
+
+        // Enough wrong answers kills the player
+        //while (!player.isDead()) {
+          //  room.attack("1+1", "99"); // always wrong
+       // }
+
+     //   assertTrue(player.isDead(),
+              //  "Player should die with too many wrong answers");
+     //   assertFalse(room.isCleared(),
+              //  "Room is not cleared if player dies before defeating the monster ");
+    //}
+//}

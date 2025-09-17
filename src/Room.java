5939c3666f98c300e9fdf296021aceef4f44d4b3
Björn <bjorn.ljunggren.5277@student.uu.se>
2025-09-17 11:52:31 +0200

    testing the testing

--- src/Room.java
+++ src/Room.java
@@ -33,6 +33,8 @@ public class Room
         exits = new HashMap<>();
     }
 
+    //Testing the testing
+
     /**
      * Define an exit from this room.
      * @param direction The direction of the exit.
